7332ca058c97861830ca0c0be0cf8b989cc9799e
aqeel parpia <aqeelparpia786@hotmail.com>
2020-04-18 20:20:27 -0400

    Mehdi alis protfolio

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { VideosComponent } from './videos/videos.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    routingComponents,
+    VideosComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
