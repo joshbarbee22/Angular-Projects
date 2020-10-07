09a263bb40fb7fa3ee98b43c22fae97c1845924d
joshbarbee22 <joshbarbee22@gmail.com>
2020-10-07 12:22:11 -0400

    Auth Page Design

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { VideosComponent } from './videos/videos.component';
+
 
 const routes: Routes = [
-    { path: '', component: HomeComponent},
-    { path: 'gallery',component: GalleryComponent},
-    { path: 'videos',component: VideosComponent},
-    { path: 'about', component: AboutComponent},
-    { path: 'contact', component: ContactComponent},
+  { path: '', component: ContactComponent}, 
 ];
   
   @NgModule({
@@ -19,5 +12,5 @@ const routes: Routes = [
     exports: [RouterModule]
   })
   export class AppRoutingModule { }
-  export const routingComponents = [HomeComponent,AboutComponent,GalleryComponent,VideosComponent,ContactComponent]
+  export const routingComponents = [ContactComponent]
   
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,19 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
-import { VideosComponent } from './videos/videos.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     routingComponents,
-    VideosComponent,
+    
     
     
   ],
